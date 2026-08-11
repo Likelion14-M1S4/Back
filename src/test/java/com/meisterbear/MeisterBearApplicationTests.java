2847e4067079760a813cf55dc60c0076649cfdda
jominho37 <jominho37@gmail.com>
2026-08-12 00:00:02 +0900

    🎉 Start: Spring Boot 프로젝트 초기 세팅

---
+++ src/test/java/com/meisterbear/MeisterBearApplicationTests.java
@@ -0,0 +1,12 @@
+package com.meisterbear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeisterBearApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
